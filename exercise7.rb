d2c273860e069218b56d6fda57ae7cc5ea12ab9b
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-13 16:48:18 -0500

    updated exercise7, removed cohort2

--- exercise7.rb
+++ exercise7.rb
@@ -1,8 +1,22 @@
 ## Hash exercise. Keys an be data types: symbols, integers, float, and strings
 
+def cohorts(students)
+  students.each do |num, value|
+    puts "#{num}: #{value} students."
+  end
+end
+
 students = {
-  :cohort1 = 34,
-  :cohort2 = 42,
-  :cohort3 = 22
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
 
 }
+  students[:cohort4] = 43
+  students.each do |num, value|
+  students[num] = (value * 1.05).round
+end
+  students.delete(:cohort2)
+  cohorts(students)
+
+  puts "#{students.keys}"
