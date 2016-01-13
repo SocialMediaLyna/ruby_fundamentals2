69cb5858448ff1a4081d26d14284db87baef57ed
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-13 15:34:59 -0500

    added rice to the grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,22 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list.each do |item|
-  puts "*#{item}."
+def write_list(grocery_list)
+  grocery_list.each do |item|  # this is a loop and block (each)
+
+    puts "*#{item}."
+
+  end
+end
+
+  grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+  grocery_list << "rice"
+  puts "added rice"
+  write_list(grocery_list)
+
+  puts "#{grocery_list.length}"
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas."
+
+else
+  puts "You don't need to pick up bananas today."
 
 end
